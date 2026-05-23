390aa157db1cb07c4d37fd07146c772dc7d14070
BT04643 <anshumandev1101@gmail.com>
2026-05-23 06:46:07 +0530

    Added React practice projects

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai" 
+function App() {
+  const username="chai aur code"
+
+  return (
+    <>
+    <h1>Hello {username}</h1>
+    <h2>hIIII</h2>
+      <Chai/>
+    </>
+  )
+}
+
+export default App
